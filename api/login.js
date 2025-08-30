5cb081c278aa4f01e69cde4c2b1baf026b2a6039
Jackjery <m13289558678@163.com>
2025-08-30 11:25:33 +0800

    Update login.js

--- api/login.js
+++ api/login.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 // 确保连接字符串正确处理
-const connectionString = process.env.POSTGRES_URL;
+const connectionString = your-random-secret-key-123 ;
 
 if (!connectionString) {
   console.error('错误: 未设置POSTGRES_URL环境变量');
@@ -118,4 +118,5 @@ export default async function handler(req, res) {
     });
   }
 }
+
 
