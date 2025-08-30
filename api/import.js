e35c5be8ba4d55bdf693f4c177bc93866029bb78
Jackjery <m13289558678@163.com>
2025-08-30 11:36:08 +0800

    Update import.js

--- api/import.js
+++ api/import.js
@@ -3,7 +3,7 @@ import { verifyAuth } from '../../lib/auth';
 
 let pool;
 if (!global._pgPool) {
-  pool = new Pool({ connectionString:connectionString: your-random-secret-key-123  });
+  pool = new Pool({ connectionString: process.env.your-random-secret-key-123   });
   global._pgPool = pool;
 } else {
   pool = global._pgPool;
@@ -198,4 +198,5 @@ export default async function handler(req, res) {
   }
 }
     
+
 
